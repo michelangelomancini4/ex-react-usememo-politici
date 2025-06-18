88c4e2fc638e588aeb777bc3cb4ca5e31b27d20d
michelangelomancini <michelangelomancini8@gmail.com>
2025-06-18 11:01:09 +0200

    Milestone 1 completed– fetch and render of politicians data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    async function politiciansData() {
+
+      try {
+
+        const response = await fetch('http://localhost:3333/politicians')
+
+        const data = await response.json();
+        console.log(data);
+        setPoliticians(data)
+
+
+
+      } catch (error) {
+
+        console.error('Errore durante la chiamata:', error);
+      }
+
+    }
+    politiciansData();
+
+
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>ESERCITAZIONE </p>
+      <h1>Politicians</h1>
+      <ul className='card-list'>
+        {politicians.map(politician => (
+          <li key={politician.id} >
+            <div className='card'>
+              <h3>{politician.name}</h3>
+              <span>{politician.position}</span>
+              <p>{politician.biography}</p>
+              <img src={politician.image} alt="Photo" />
+            </div>
+          </li>
+        ))}
+      </ul>
+
     </>
   )
 }
 
 export default App
+
+//  Milestone 1: Recuperare e visualizzare i dati
+// Effettua una chiamata API a
+// /politicians
+
+// Salva la risposta in uno stato React (useState).
+
+// Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+
+// Nome (name)
+// Immagine (image)
+// Posizione (position)
+// Breve biografia (biography)
+
+// Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
